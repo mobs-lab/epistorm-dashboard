5517e1c6d19a7124ee0d9e194745c8ae57f17585
Bote Wang <wang.bot@northeastern.edu>
2024-02-22 17:04:23 -0500

    Set up routes, TODO: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
 import {Metadata} from 'next'
-
-export const metadata: Metadata = {
-    //TODO: metadata
-    title: 'Epistorm Dashboard',
-    description: 'Dashboard for visualization of _______ data',
-
-
-
-}
-
-
 export default function Page() {
     return '...'
 }
