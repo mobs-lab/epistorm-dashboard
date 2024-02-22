a5585bce509936018e22fbc87eb0ba4145de2922
Bote Wang <wang.bot@northeastern.edu>
2024-02-22 16:37:03 -0500

    added css, lots of TODOs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,15 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
+        //TODO: Define how the whole top-level layout should look like.
+        // Using Holy Grail layout, header should always be the navigation bar
+        // Footer should always be the same as well
+        // The main content are dynamic, depending on the route
         <html lang="en">
+        <header>
+
+
+        </header>
         <body>{children}</body>
         </html>
     )
