a01cf979eca20eee33d47423000c22c396f18aac
Bote Wang <wang.bot@northeastern.edu>
2024-02-23 15:15:33 -0500

    added more ui

--- src/app/forecasts/page.tsx
+++ src/app/forecasts/page.tsx
@@ -1,12 +1,20 @@
+import FiltersPane from '../Components/dashboard/FiltersPane';
+import ForecastChart from '../Components/dashboard/ForecastChart';
+import SettingsPane from '../Components/dashboard/SettingsPane';
 
 
 export default function Page() {
 
-    return (
-        <div>
-            <div>
-                {/*<InfoPage/>*/}
-            </div>
+    return (<div className={"flex flex-row px-2 flex-nowrap"}>
+        <div className={"flex-none basis-1/5"}>
+            <FiltersPane/>
         </div>
-    )
-}
+        <div className={"flex-auto basis-4/5"}>
+
+            <ForecastChart/>
+        </div>
+        <div className={"hidden"}>
+            <SettingsPane/>
+        </div>
+    </div>)
+};
