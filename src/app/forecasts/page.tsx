4360e82a507a57d5b3e0f37c061eecd01a882439
Bote Wang <wang.bot@northeastern.edu>
2024-03-12 09:33:55 -0400

    update to line graph component

--- src/app/forecasts/page.tsx
+++ src/app/forecasts/page.tsx
@@ -4,25 +4,28 @@ import RiskLevelGauge from "../Components/dashboard/RiskLevelGauge";
 import LineChart from "../Components/dashboard/ForecastChart";
 import StateMapWithFilters from "../Components/dashboard/FiltersPane";
 
-
 export default function Page() {
 
     return (
-
-        <div className="container mx-auto p-6">
-            <div className="grid grid-cols-3 gap-4">
-                <div className="col-span-2">
-                    {/* State details and risk gauge side-by-side */}
-                    <div className="grid grid-cols-2 gap-4">
-                        <StateDetail stateName="MA" hospitalizations={1234}/>
-                        <RiskLevelGauge riskLevel="moderate"/>
-                    </div>
-                    {/* Line chart below */}
-                    <LineChart /* chart data props */ />
+        <div className={"container mx-auto p-2"}>
+            <div id={"dashboard-grid-layout"}>
+                <div className={"grid-area: state"}>
+                    <h1> State </h1>
                 </div>
-                {/* Right-hand side map and filters */}
-                <StateMapWithFilters /* props */ />
+                <div className={"grid-area: gauge"}>
+                    <h1> Gauge </h1>
+                </div>
+                {/* Line chart below */}
+                <div className={"grid-area: graph"}>
+                    <h1> Graph </h1>
+
+                </div>
+                <div className={"grid-area: settings"}>
+                    <h1> Settings Pane</h1>
+
+                </div>
             </div>
         </div>
+
     )
 };
