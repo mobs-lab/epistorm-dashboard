8b6b6ec2075662287000055b99f6002dc7f3370f
Bote Wang <wang.bot@northeastern.edu>
2024-03-13 14:25:05 -0400

    modified forecast charts

--- src/app/Components/dashboard/ForecastChart.tsx
+++ src/app/Components/dashboard/ForecastChart.tsx
@@ -4,18 +4,34 @@
 import React, {useRef} from "react";
 import * as d3 from "d3";
 
-const mobs_gleam_data = "/data/processed/";
+const mobs_gleam_data = "~/data/processed/MIGHTE-Nsemble/predictions.csv";
 
 type LineChartProps = {
     // Props for your data
 };
 
-const predictionData = await d3.csv(",", );
+// TODO Read the predictions data into a const
+// const predictionData = await d3.csv(",", mobs_gleam_data );
 
+// TODO Construct a chart, configure it;
 const LineChart: React.FC<LineChartProps> = () => {
+    // reference to svg object
     const svgRef = useRef(null);
-    const lineChart = d3.select(svgRef.current).append("svg").attr("width", 960).attr("height", 720);
 
+    // Set up size and margins
+    const width = 928;
+    const height = 500;
+
+    const marginTop = 20;
+    const marginBottom = 20;
+    const marginLeft = 20;
+    const marginRight = 20;
+
+    // Build X-Axis, which is based on dates
+    const x = d3.scaleUtc(d3.extent )
+
+
+    // Return the SVG object using reference
     return (<svg ref={svgRef}></svg>);
 };
 
