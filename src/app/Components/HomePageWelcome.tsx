e02a4b820f6219194572f93c5738451e1da6a18e
Bote Wang <wang.bot@northeastern.edu>
2024-03-14 14:19:34 -0400

    Updated workflow to retrieve ground truth data

--- src/app/Components/HomePageWelcome.tsx
+++ src/app/Components/HomePageWelcome.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePageWelcome: React.FC = () => {
     return (
@@ -7,13 +8,13 @@ const HomePageWelcome: React.FC = () => {
                 className="flex-grow flex items-center justify-center"> {/* Centering text vertically and horizontally */}
                 <p className="text-xl text-center">Welcome to Epistorm Dashboard!</p>
             </div>
-            <div className="flex flex-grow flex-col space-x-4 pb-4 px-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <div className="flex justify-center space-x-4 ">
+                <Link href={"/forecasts"} className=" w-1/6 bg-blue-500 hover:bg-blue-700 text-white p-2 font-bold rounded">
                     View Forecasts
-                </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                </Link>
+                <Link href={"/background"} className="w-1/6 bg-blue-500 hover:bg-blue-700 text-white p-2 font-bold rounded">
                     About us
-                </button>
+                </Link>
             </div>
         </div>
     )
--- src/app/evaluations/page.tsx
+++ src/app/evaluations/page.tsx
@@ -1,14 +1,33 @@
-import InfoPage from "../Components/InfoPage";
+import React from "react";
+import StateDetail from "../Components/dashboard/SingleState";
+import RiskLevelGauge from "../Components/dashboard/RiskLevelGauge";
+import LineChart from "../Components/dashboard/ForecastChart";
+import StateMapWithFilters from "../Components/dashboard/FiltersPane";
 
 export default function Page() {
 
     return (
-        <div>
-            <h1> Evaluations </h1>
-            <hr/>
-            <div>
-                <InfoPage/>
+        <div className={"container mx-auto"}>
+            <div className={"evaluations-grid-layout"}>
+                <div className={"bg-yellow-500 evaluations-title"}>
+                    <h1> Model Evaluations</h1>
+                    <h3> This page shows a smaller version of line chart, side by side with the evaluations based on ___ scoring system.</h3>
+                </div>
+                <div className={"bg-amber-800 evaluations-graph"}>
+                    <h1>
+                        Model Graph
+                    </h1>
+                    <LineChart/>
+                </div>
+                {/* Line chart below */}
+                <div className={"bg-amber-900 evaluations-score"}>
+
+                </div>
+                <div className={"bg-fuchsia-300 evaluations-settings"}>
+
+                </div>
             </div>
         </div>
+
     )
-}
+};
