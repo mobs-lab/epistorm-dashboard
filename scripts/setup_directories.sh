961fd626b95e9cc251177530ed8b3b194a991f13
Bote Wang <wang.bot@northeastern.edu>
2025-01-16 02:45:03 -0500

    added new models (#139)

--- scripts/setup_directories.sh
+++ scripts/setup_directories.sh
@@ -6,6 +6,9 @@ mkdir -p public/data/processed/MIGHTE-Nsemble
 mkdir -p public/data/processed/NU_UCSD-GLEAM_AI_FLUH
 mkdir -p public/data/processed/CEPH-Rtrend_fluH
 mkdir -p public/data/processed/FluSight-ensemble
+mkdir -p public/data/processed/MIGHTE-Joint
+mkdir -p public/data/processed/NEU_ISI_AdaptiveEnsemble
+mkdir -p public/data/processed/NEU_ISI-FluBcast
 
 # For historical ground truth
 mkdir -p public/data/ground-truth/historical-data
