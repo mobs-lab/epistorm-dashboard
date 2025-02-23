14b49d240313d948e3addbd36e9ace50e704e4a6
Bote Wang <wang.bot@northeastern.edu>
2025-02-23 17:01:50 -0500

    fix to import paths (#155)

--- src/app/store/hooks.ts
+++ src/app/store/hooks.ts
@@ -1,6 +1,6 @@
 // src/app/store/hooks.ts
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './index';
+import type { RootState, AppDispatch } from './index.ts';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
