bb13ee0a88fb10d0f9822139e44188759b4ef033
Bote (Porter) Wang <wang.bot@northeastern.edu>
2025-03-23 16:37:12 -0400

    added flag-based feature showing for evaluations page

---
+++ src/app/utils/featureFlag.ts
@@ -0,0 +1,27 @@
+// src/app/utils/featureFlags.ts
+export type FeatureFlag = 'seasonOverviewTab' | string;
+
+/**
+ * Utility to check if a feature should be enabled based on environment
+ * Note: this implementation stills bundles disabled features, change to conditional imports when needed (though that will depend on Next.js version)
+ */
+export const isFeatureEnabled = (feature: FeatureFlag): boolean => {
+  // Depends on app.yaml or app.demo.yaml, which are determined by branches + CI/CD pipeline
+  const environment = process.env.NEXT_PUBLIC_GAE_SERVICE || 'default';
+  
+  // Define which features are only available in the demo environment
+  const demoOnlyFeatures: FeatureFlag[] = [
+    'seasonOverviewTab',
+	/* TODO: Flag the layout change (CSS) for the nowcast widgets */
+	
+    // Add other experimental features here as needed
+  ];
+
+  // For demo-only features, only enable them in the demo environment
+  if (demoOnlyFeatures.includes(feature)) {
+    return environment === 'demo';
+  }
+
+  // Default to enabled for any other features
+  return true;
+};
