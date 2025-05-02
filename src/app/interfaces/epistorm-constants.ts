bea1f4c2ad156e6806697054905e19227cf02cd7
Bote (Porter) Wang <wang.bot@northeastern.edu>
2025-05-02 19:48:58 -0400

    update to SO statemap color and tooltip

--- src/app/interfaces/epistorm-constants.ts
+++ src/app/interfaces/epistorm-constants.ts
@@ -1,14 +1,14 @@
 // Color Mapping for each model
 
 export const modelColorMap: Record<string, string> = {
-    "MOBS-GLEAM_FLUH": "#9ee09b",
-    "MIGHTE-Nsemble": "#5eddcb",
-    "CEPH-Rtrend_fluH": "#22a1e0",
-    "NU_UCSD-GLEAM_AI_FLUH": "#8181ef",
-    "FluSight-ensemble": "#fe620e",
-    "MIGHTE-Joint": "#5effcb",
-    "NEU_ISI-AdaptiveEnsemble": "#ff0000",
-    "NEU_ISI-FluBcast": "#ffeaae",
+    "MOBS-GLEAM_FLUH": "#9CEB94",
+    "MIGHTE-Nsemble": "#95F4C9",
+    "MIGHTE-Joint": "#3FC49E",
+    "NU_UCSD-GLEAM_AI_FLUH": "#45CDED",
+    "CEPH-Rtrend_fluH": "#0292D1",
+    "NEU_ISI-FluBcast": "#7BB1FF",
+    "NEU_ISI-AdaptiveEnsemble": "#5F5FD6",
+    "FluSight-ensemble": "#D36F54",
 //     Add more here in the future...
 };
 
